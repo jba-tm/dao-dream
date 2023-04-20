38e850a6f737e4fefd92d9a9a5cb9b3872980999
selim <selimdev00@gmail.com>
2023-04-20 14:54:29 +0500

    [button bg]: transparent

--- src/components/LearnMoreButton.jsx
+++ src/components/LearnMoreButton.jsx
@@ -1,6 +1,6 @@
 export default function LearnMoreButton() {
   return (
-    <button className="py-2 px-4 rounded-full bg-border hover:opacity-50 ease-linear duration-150">
+    <button className="learn-more-button py-2 px-4 rounded-full hover:opacity-50 ease-linear duration-150">
       Learn more
     </button>
   );
